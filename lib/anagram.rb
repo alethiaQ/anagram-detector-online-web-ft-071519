ab3a9847e7db165507bf3772f1fa80e6427887f8
Alethia Quintero <alethiaq22@gmail.com>
2019-07-26 20:31:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,22 +5,23 @@ attr_accessor :word
 
   def initialize(word)
     @word = word
-
+    @matches =[]
   end
 
   def match(anagram_array)
     anagram_array.each do |word|
-      matches =[]
+
       split_arr_word = word.split("").sort
       split_word = @word.split("").sort
 
       if split_arr_word == split_word
-        matches << word
+        @matches << word
       else
         return empty = []
-
       end
+
     end
+    @matches
   end
 
 
