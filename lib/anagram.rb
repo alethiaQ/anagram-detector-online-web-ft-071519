75cb8277a8e8e3d47d100eebb749e4c2e52a51aa
Alethia Quintero <alethiaq22@gmail.com>
2019-07-26 20:16:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,25 @@
 # Your code goes here!
 class Anagram
+attr_accessor :word
+
 
   def initialize(word)
+    @word = word
 
   end
+
+  def match(anagram_array)
+    anagram_array.each do |word|
+      split_arr_word = word.split("")
+      split_word = @word.split("")
+
+      if split_word.sort == split_arr_word.sort
+        word
+      else
+        return empty = []
+      end 
+    end
+  end
+
 
 end
