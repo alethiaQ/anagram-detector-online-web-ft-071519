c4befc8a1839b3b220d642629124dcf7b5be3631
Alethia Quintero <alethiaq22@gmail.com>
2019-07-26 20:21:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,7 +14,7 @@ attr_accessor :word
       split_word = @word.split("").sort
 
       if split_arr_word == split_word
-        return word
+        return anagram_array[word]
       else
         return empty = []
 
